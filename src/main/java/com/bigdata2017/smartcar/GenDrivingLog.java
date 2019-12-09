86f2aff9a4434d0cb830e34e2b67405ac6b03c4b
bit <bit@DESKTOP-TJ4POIC>
2019-12-09 17:25:50 +0900

    log path 수정

--- src/main/java/com/bigdata2017/smartcar/GenDrivingLog.java
+++ src/main/java/com/bigdata2017/smartcar/GenDrivingLog.java
@@ -43,7 +43,7 @@ public class GenDrivingLog {
 			int tmpWildCarNum;
 	
 			//printWriter = new PrintWriter( System.out, true );
-			String logFile = "./logs/driving/driving.log";
+			String logFile = "D:\\carlog/car.log";
 			printWriter = new PrintWriter( new FileWriter( logFile ), true );
 			
 			for(int i = 1; i <= carCount; i++) {
